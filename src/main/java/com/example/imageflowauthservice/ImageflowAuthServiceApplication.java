5baa3a2a12d4987083117b86583bb6612efea1a1
Giancotsu <gianfrancolarocca2797@gmail.com>
2023-02-27 22:34:12 +0100

    Imageflow authentication service initialized.

---
+++ src/main/java/com/example/imageflowauthservice/ImageflowAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageflowauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageflowAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageflowAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imageflowauthservice/ImageflowAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageflowauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageflowAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
