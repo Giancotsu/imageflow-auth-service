527f7161ffde321958051a134b0474881cc8eec5
Giancotsu <gianfrancolarocca2797@gmail.com>
2023-02-28 21:59:26 +0100

    Adding UserController

---
+++ src/main/java/com/example/imageflowauthservice/controller/UserController.java
@@ -0,0 +1,85 @@
+package com.example.imageflowauthservice.controller;
+
+import com.example.imageflowauthservice.model.Profile;
+import com.example.imageflowauthservice.model.User;
+import com.example.imageflowauthservice.payload.UserSummary;
+import com.example.imageflowauthservice.service.UserService;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.ResourceNotFoundException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@Slf4j
+@RequestMapping("users")
+public class UserController {
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> findUser(@PathVariable("username") String username) {
+        log.info("retrieving user {}", username);
+
+        return  userService
+                .findByUsername(username)
+                .map(user -> ResponseEntity.ok(user))
+                .orElseThrow(() -> new ResourceNotFoundException(username));
+    }
+
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> findAll() {
+        log.info("retrieving all users");
+
+        return ResponseEntity
+                .ok(userService.findAll());
+    }
+
+    @GetMapping(value = "/summary/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserSummary(@PathVariable("username") String username) {
+        log.info("retrieving user {}", username);
+
+        return  userService
+                .findByUsername(username)
+                .map(user -> ResponseEntity.ok(convertTo(user)))
+                .orElseThrow(() -> new ResourceNotFoundException(username));
+    }
+
+    @PostMapping(value = "/summary/in", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserSummaries(@RequestBody List<String> usernames) {
+        log.info("retrieving summaries for {} usernames", usernames.size());
+
+        List<UserSummary> summaries =
+                userService
+                        .findByUsernameIn(usernames)
+                        .stream()
+                        .map(user -> convertTo(user))
+                        .collect(Collectors.toList());
+
+        return ResponseEntity.ok(summaries);
+
+    }
+
+    private UserSummary convertTo(User user) {
+        return UserSummary
+                .builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .name(user.getUserProfile().getDisplayName())
+                .profilePicture(user.getUserProfile().getProfilePictureUrl())
+                .build();
+    }
+
+}
